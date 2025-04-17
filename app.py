54b398e5ddcc23391b6bcead14d9291f0e138a32
Hari Sri <142750864+Hari-Sri-T@users.noreply.github.com>
2025-04-17 12:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 # Your Gradio backend URL (no trailing slash)
-GRADIO_BACKEND_URL = "https://d2b8-2402-e280-3e0b-7b4-3cbf-8704-ca99-3838.ngrok-free.app"
+GRADIO_BACKEND_URL = "https://cbb8-122-15-132-69.ngrok-free.app"
 
 
 @app.route("/")
